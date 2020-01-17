c0cfd19e8e15d0d54388ed4bfaf3c4d88c6f0178
Rachel Carranza <rachel.v.carranza@gmail.com>
2020-01-17 00:23:37 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -31,25 +31,25 @@ if (age>=13 && age<=19)  { return "You are a teenager";}
 
 function switchAge(age) {
   switch(age){
-  case age=13:
+  case 13:
      return "You are a teenager";
     break;
-    case age=14:
+    case 14:
        return "You are a teenager";
       break;
-      case age=15:
+      case 15:
          return "You are a teenager";
         break;
-        case age=16:
+        case 16:
            return "You are a teenager";
           break;
-          case age=17:
+          case 17:
              return "You are a teenager";
             break;
-            case age=18:
+            case 18:
                return "You are a teenager";
               break;
-              case age=19:
+              case 19:
                 return "You are a teenager";
                 break;
                 default:
