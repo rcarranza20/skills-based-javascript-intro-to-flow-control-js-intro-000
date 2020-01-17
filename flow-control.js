053d2f4c6c5baae82b55e8d56e87cb1edfb1e20f
Rachel Carranza <rachel.v.carranza@gmail.com>
2020-01-17 00:13:48 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -24,7 +24,7 @@ else if (age<=12){
 }
 
 function ternaryTeenager(age) {
-  (if age>=13 && age<=19) ? return "You are a teenager" : return "You are not a teenager";
+ return (if age>=13 && age<=19) ? "You are a teenager" : "You are not a teenager";
 
 }
 
